0ebc8cd8ffede4f0d5284ba28f4e511382b4ce30
Greg Johnston <greg.a.johnston@gmail.com>
2016-05-17 15:41:55 -0400

    Created inital model logic

--- questionboard/models.py
+++ questionboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Answer(models.Model):
+    owner = models.ForeignKey('auth.User', default='')
+    created = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(default='')
+    score = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('score', )
+
+
+class Question(models.Model):
+    answer = models.ForeignKey('Answer', default=1)
+    owner = models.ForeignKey('auth.User', default=1)
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255, blank=True, default='')
+    description = models.TextField(default='')
+
+    class Meta:
+        ordering = ('created', )
