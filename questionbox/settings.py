2f5f9142e3d2a432a32870931123b9da7c980e04
David Conley <conleydg@gmail.com>
2016-05-17 15:05:12 -0400

    secret problem

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = {'database':{}}
+DATABASES = {'default':{}}
 DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
