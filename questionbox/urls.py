1c89c009c5f5a8fe052e50074166cd4ed1eb876d
Greg Johnston <greg.a.johnston@gmail.com>
2016-05-18 03:13:08 -0400

    Several small fixes; hookup for basic JS; not complete

--- questionbox/urls.py
+++ questionbox/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework import routers
-from questionbox import views
+from rest_framework.routers import DefaultRouter
+from questionboard import views
 
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'question', views.QuestionViewSet)
 router.register(r'answer', views.AnswerViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'questionbox.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
 ]
