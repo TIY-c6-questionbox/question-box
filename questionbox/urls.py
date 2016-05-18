5b1d7be627d1a2acd28ad38157fb9f56b6c0712b
David Conley <conleydg@gmail.com>
2016-05-17 20:35:31 -0400

    fixing url typo

--- questionbox/urls.py
+++ questionbox/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'questionbox.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^admin/', admin.site.urls),
+
     url(r'^admin/', include(admin.site.urls)),
 ]
