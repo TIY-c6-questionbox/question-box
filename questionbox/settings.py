6801027f60bec104d6fdb9a1e399ea83d82e9bad
Trisha <ptaylor403@gmail.com>
2016-05-19 16:38:33 -0400

    answer model changed

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -105,7 +105,8 @@ USE_TZ = True
 
 
 # Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
+path_url = os.environ.get('QUESTION_DATABASE_URL')
+db_from_env = dj_database_url.config(default=path_url, conn_max_age=500)
 DATABASES = {'default': {}}
 DATABASES['default'].update(db_from_env)
 
